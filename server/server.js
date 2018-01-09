faea7bbfb9243934fd3e4ac215e33b1748893552
Brandon Tran <brandon@Brandons-MacBook-Air.local>
2018-01-09 13:03:07 +0800

    added socket.io"

--- server/server.js
+++ server/server.js
@@ -1,15 +1,25 @@
 const path = require('path');
+const http = require('http');
+
+const socketIO = require('socket.io');
 const publicPath = path.join(__dirname, '../public');
 
 const express = require('express');
 var port = process.env.PORT || 3000;
 
 var app = express();
-
+var server = http.createServer(app);
 //app.use(bodyParser.json());
+var io = socketIO(server);
 
+io.on('connection', (socket) => {
+  console.log('New user connected');
+  socket.on('disconnect', () => {
+    console.log('Client disconnected');
+  });
+});
 app.use(express.static(publicPath));
 
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`Listening on port ${port}`);
 })
