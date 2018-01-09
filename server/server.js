8b3bf8e9a4dc9fa1441213e5137503d58e793a72
Brandon Tran <brandon@Brandons-MacBook-Air.local>
2018-01-09 12:33:54 +0800

    inital commit

---
+++ server/server.js
@@ -0,0 +1,15 @@
+const path = require('path');
+const publicPath = path.join(__dirname, '../public');
+
+const express = require('express');
+var port = process.env.PORT || 3000;
+
+var app = express();
+
+//app.use(bodyParser.json());
+
+app.use(express.static(publicPath));
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+})
